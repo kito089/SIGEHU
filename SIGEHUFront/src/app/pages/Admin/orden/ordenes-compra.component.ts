a2a652b23aed76a819968ce3659644e6b7ce4696
kito089 <jezarinan@gmail.com>
2026-08-06 09:08:12 -0600

    cosas raras del merge

--- SIGEHUFront/src/app/pages/Admin/orden/ordenes-compra.component.ts
+++ SIGEHUFront/src/app/pages/Admin/orden/ordenes-compra.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
